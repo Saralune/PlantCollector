34311d1d8f3566c2feab41e0f75a66ecf8d03231
saralune <sarahlef59@hotmail.fr>
2024-11-11 10:47:58 +0100

    Initial Commit

---
+++ PlantCollector/PlantCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantCollectorApp.swift
+//  PlantCollector
+//
+//  Created by Sara Lefort on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlantCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
